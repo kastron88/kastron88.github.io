b82b173a4a0c3554e5d5a5e8ba568112c66e7c94
jorge.jimenez-garcia <jorge.jimenez-garcia@akka.eu>
2020-04-13 19:27:50 +0200

    JJG
    First CV online approach

---
+++ actions.js
@@ -0,0 +1,47 @@
+﻿import i18nKeyValues from "./i18n.js";
+const i18nElements = document.querySelectorAll('[data-i18n]');
+const defaultLanguage = "es";
+const languageList = {
+    "en": "English",
+    "es": "español"
+}
+
+const getTextForElement = (key, lang) => {
+    let text = i18nKeyValues[key][lang];
+    if(text == undefined){
+        console.warn(`'${key}' text key doesn't have a translation for '${lang}' language`);
+    }
+    return text == undefined ? i18nKeyValues[key][defaultLanguage] : text;
+}
+
+if(document.getElementById('en') !== null){
+	const en = document.getElementById('en');
+	en.onclick = () => changeLang('en');
+}
+if(document.getElementById('es') !== null){
+	const es = document.getElementById('es');
+	es.onclick = () => changeLang('es');
+}		
+
+const startLangSelector = () => {
+	let browserLanguage = navigator.language.split("-")[0];
+	if(localStorage.getItem("lang") !== null){
+		browserLanguage = localStorage.getItem("lang");
+	}else{
+		if(!(browserLanguage in languageList)){
+			browserLanguage = defaultLanguage;
+		}
+	}		
+	for(let el of i18nElements){
+			el.innerHTML = getTextForElement(el.getAttribute("data-i18n"), browserLanguage);
+	}
+}
+
+const changeLang = (lang) => {
+	localStorage.setItem("lang", lang);
+    for(let el of i18nElements){
+        el.innerHTML = getTextForElement(el.getAttribute("data-i18n"), lang);
+    }
+};
+
+startLangSelector();
---
+++ i18n.js
@@ -0,0 +1,219 @@
+﻿const i18nKeyValues = {
+    "home": {
+        "en": "INDEX",
+        "es": "INICIO"
+    }, 
+    "personal-data": {
+        "en": "PERSONAL DATA",
+        "es": "DATOS PERSONALES"
+    },
+    "academic-training": {
+        "en": "ACADEMIC TRAINING",
+        "es": "FORMACION ACADÉMICA"
+    },
+    "additional-training": {
+        "en": "ADDITIONAL TRAINING",
+        "es": "FORMACIÓN ADICIONAL"
+    },
+    "work-experience": {
+        "en": "WORK EXPERIENCE",
+        "es": "EXPERIENCIA PROFESIONAL"
+    },
+    "computer-literacy": {
+        "en": "COMPUTER LITERACY",
+        "es": "CONOCIMIENTOS INFORMÁTICOS"
+    },
+    "languages": {
+        "en": "LANGUAGES",
+	"es": "IDIOMAS"
+    },
+    "social-skills": {
+        "en": "SOCIAL SKILLS",
+        "es": "HABILIDADES SOCIALES"
+    },
+    "other-facts": {
+        "en": "OTHER INTERESTING FACTS",
+        "es": "OTROS DATOS"
+    },
+    "first-name": {
+        "en": "First name:",
+        "es": "Nombre:"
+    },
+    "first-name-value": {
+        "en": "Jorge",
+        "es": "Jorge"
+    },
+    "surname": {
+        "en": "Surname:",
+        "es": "Apellidos:"
+    },
+    "surname-value": {
+        "en": "Jiménez",
+        "es": "Jiménez García"
+    },
+    "birthdate": {
+        "en": "Birthdate:",
+        "es": "Fecha de nacimiento:"       
+    },
+    "birthdate-value": {
+        "en": "19/08/1988",
+		"es": "19/08/1988"
+	},   
+    "country": {
+        "en": "Country",
+        "es": "País:"
+    },
+    "country-value": {
+        "en": "Spain",
+        "es": "España"
+    },
+    "at-uclm": {
+        "en": "2006-2011 Degree in chemical. Made in the Faculty of Chemical Sciences of Ciudad Real, University of Castilla La Mancha (UCLM), Spain.",
+        "es": "2006-2011 Licenciatura en químicas. Realizado en la facultad de Ciencias Químicas de Ciudad Real, Universidad de Castilla La Mancha (UCLM)." 
+    },
+    "at-uah": {
+        "en": "2011-2012 “Quimica Inorgánica Molecular” master’s degree. Made in the University of Alcalá de Henares (UAH).",
+        "es": "2011-2012 Máster en “Quimica Inorgánica Molecular” master’s degree. Realizado en la Universidad de Alcalá de Henares (UAH)."
+    },
+    "at-cibersecurity": {
+        "en": "March – July 2017. IT Security training course founded by Madrid government. Duration: 500 h.",
+        "es": "Marzo – Julio 2017. Curso en Seguridad Informática financiado por la comunidad de Madrid. Impartido en el centro SERPROFES de San Sebastián de los Reyes. Duración: 500 h."
+    },
+    "add-social-skills": {
+        "en": "October 2010 Workshop on Communication and Social Skills conducted at the Center for Information and Employment Promotion of the University of Castilla La Mancha.",
+        "es": "Octubre 2010 Taller de Comunicación y Habilidades Sociales realizado en el Centro de Información y Promoción del Empleo (CIPE) de la Universidad de Castilla La Mancha."
+    },
+    "add-oracle": {
+        "en": "February 2015. Oracle 11g training course. Duration: 18 h.",
+		"es": "Febrero 2015. Curso de oracle 11g. Duración: 18 h."
+    },
+    "add-servlets": {
+        "en": "September 2015. Application development with servlets and JSP training course. Duration 20 h.",
+        "es": "Septiembre 2015. Curso de Desarrollo de aplicaciones con servlets y JSP. Duración 20 h."
+    },
+    "add-plsql": {
+        "en": "May 2016. Oracle 11g PL/SQL training course. Duration: 18 h.",
+        "es": "Mayo 2016. Curso oracle 11g PL/SQL. Duración: 18 h."
+    },
+    "add-analysis": {
+        "en": "July 2016. UML and object oriented Analysis and Design training course. Duration 15 h.",
+        "es": "Julio 2016. Curso de Análisis y diseño UML orientado a objetos. Duración 15 h."
+    },
+    "add-plsql2": {
+        "en": "October 2016. Oracle 11g PL/SQL training course. Duration: 12 h.",
+        "es": "Octubre 2016. Curso de Oracle 11g PL/SQL. Duración: 12 h."
+    },
+    "add-jsf": {
+        "en": "October 2016. JSF training course. Duration: 20 h.",
+        "es": "Octubre 2016. Curso de JSF. Duración: 20 h."
+    },
+    "add-spring": {
+        "en": "October 2016. Spring and Hibernate application development training course. Duration: 30 h.",
+        "es": "Octubre 2016. Curso de Desarrollo de aplicaciones usando Spring e Hibernate. Duración: 30 h."
+    },
+    "add-jira": {
+        "en": "July 2017 JIRA tool online training course. Duration: 8 h.",
+        "es": "Julio 2017 Curso online JIRA tool. Duración: 8 h."
+    },
+    "add-angular": {
+        "en": "September 2017: Angular 2.0 training course. Duration: 30 h.",
+        "es": "Septiembre 2017: Curso en Angular 2.0. Duración: 30 h."
+    },
+    "we-baque": {
+        "en": "March 2014-February 2015 Working at R&D department in Lácteas García-Baquero.",
+        "es": "Marzo 2014-Febrero 2015 Trabajando en el departamento de I+D en Lácteas García-Baquero."
+    },
+    "we-indra": {
+        "en": "February 2015-June 2018 Working as Junior Software Engineer at Indra Sistemas S.A. Collaborating developping web applications coded in Java language with a framework based on Struts in a public project for spanish government.",
+        "es": "Febrero 2015-Junio 2018 Trabajando como Ingeniero de Software Junior en Indra Sistemas S.A. Colaboré en el desarrollo de aplicaciones web en Java para un proyecto público de la Gerencia Informática de la Seguridad Social (GISS), utilizando un framework propietario del cliente basado en Struts."
+    },
+    "we-akkademy": {
+        "en": "June 2018 – June 2019 Working as international consultant in Akkademy. First month learning consultant “soft skills“ in Geneva and working the rest of the year in Lyon (France). Being part of AKKA research sector, collaborating in AKKA TRACE project to achieve the goal of building a prototype autonomous car (poc). Also working in a French client in the mobility environment (SPIE).",
+        "es": "Junio 2018 – Junio 2019 Consultor internacional en Akkademy. Realicé un primer mes de formación en “soft skills“ para consultores en Ginebra. El resto del año destinado en Lyon (Francia). Formé parte del departamento de investigación de Akka, colaborando en el proyecto TRACE con el objetivo de conseguir un prototipo de coche autónomo. También trabajé para el cliente SPIE del sector de mobilidad."
+    },
+    "we-akka": {
+        "en": "July 2019- Nowadays Working in AKKA Spain integrating the software development team, giving support to several group web tools and working with railway clients such as Talgo and Alstom.",
+        "es": "Julio 2019- Actualidad Trabajando en Akka España formando parte del centro de desarrollo de sofware, dando soporte a las aplicaciones web corporativas así como trabajando con clientes importantes del sector ferroviario como Talgo o Alstom."
+    },
+    "cl-office": {
+        "en": "Microsoft office (Word, Excel, Powerpoint, Access).",
+		"es": "Microsoft office (Word, Excel, Powerpoint, Access)."
+    },
+    "cl-languages": {
+        "en": "Java (J2EE), C#, C++.",
+        "es": "Java (J2EE), C#, C++."
+    },
+    "cl-frameworks": {
+        "en": "Spring MVC, .NET framework.",
+        "es": "Spring MVC, framework .NET."
+    },
+    "cl-frameworks-db": {
+        "en": "Hibernate, Entity framework.",
+        "es": "Hibernate, Entity framework."
+    },
+    "cl-db": {
+        "en": "Oracle11g, MySQL, SQL server, PostgreSQL.",
+        "es": "Oracle11g, MySQL, SQL server, PostgreSQL."},
+    "cl-xsl": {
+        "en": "xml, xsl.",
+        "es": "xml, xsl."
+    },
+    "cl-css": {
+        "en": "HTML5, CSS3.",
+        "es": "HTML5, CSS3.",
+    },
+    "cl-js": {
+        "en": "JavaScript, jQuery, bootstrap, Angular TypeScript.",
+        "es": "JavaScript, jQuery, bootstrap, Angular TypeScript."
+    },
+    "cl-uml": {
+        "en": "UML.",
+        "es": "UML."
+    },
+    "lan-english": {
+        "en": "English: Official Language School of Alcazar de San Juan (2001-2005). Equivalent to B2 level. Course held at Williams College in London for three weeks in July 2011, thanks to language grant awarded by the “Ministerio de Educación y Ciencia” (MEC).",
+        "es": "Inglés: Realizado en la Escuela Oficial de Idiomas de Alcazar de San Juan (2001-2005). Equivalente al nivel B2. Curso en el Williams College de Londres durante tres semanas en Julio de 2011, gracias a una beca del “Ministerio de Educación y Ciencia” (MEC)."
+    },
+    "lan-french": {
+        "en": "French: Official Language School of Alcazar de San Juan. Equivalent to B1 level. Living in France a whole year.",
+        "es": "Francés: Curso realizado en la Escuela Oficial de Idiomas de Alcázar de San Juan. Equivalente a B1. Residiendo en Francia durante un año."
+    },
+    "lan-deustch": {
+        "en": "Deustch: Official Language School of Alcazar de San Juan. Equivalent to A1 level.",
+        "es": "Alemán: Curso realizado en la Escuela Oficial de Idiomas de Alcázar de San Juan. Equivalente a A1."
+    },
+    "ss-prod": {
+        "en": "Productivity capabilities under pressure.",
+        "es": "Acostumbrado a trabajar bajo presión."
+    },
+    "ss-team": {
+        "en": "Teamwork skills.",
+		"es": "Trabajo en equipo."
+	},
+    "ss-pro": {
+        "en" : "Proactive person.",
+		"es" : "Proactivo."
+    },
+    "ss-goals": {
+        "en": "Used to work for goals.",
+		"es": "Acostumbrado a trabajar por objetivos."
+    },
+    "ss-time": {
+        "en": "Time self-management skills.",
+        "es": "Habilidades en autogestión del tiempo."
+    },
+    "other-imm": {
+        "en": "Immediate incorporation possibility.",
+		"es": "Posibilidad de incorporación inmediata."
+    },
+    "other-time": {
+        "en": "Time availability.",
+		"es": "Disponibilidad de tiempo."
+    },
+    "other-car": {
+        "en": "Car driving license.",
+        "es": "Carné de conducir B."
+    }
+}
+
+export default i18nKeyValues;
